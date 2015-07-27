97bb713759834d33e904199b96e7fe07ddb1ac2e
andrearaq <andrearaq@hotmail.com>
2015-07-27 13:51:47 +0200

    despliegue con postgres

--- models/models.js
+++ models/models.js
@@ -4,9 +4,7 @@ var Sequelize = require('sequelize');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
-var datosDB= "postgres://dkpsnniecxsemv:WFIlPZkKRfVDg-BOr-wKIL3EAd@ec2-107-21-125-143.compute-1.amazonaws.com:5432/d8ss2uto665bem";
-//var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var url = datosDB.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user     = (url[2]||null);
 var pwd      = (url[3]||null);
@@ -15,10 +13,10 @@ var dialect  = (url[1]||null);
 var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
-
+/*
 // Usar BBDD SQLite
 var sequelize = new Sequelize(null, null, null,
-	{dialect: "sqlite", storage: "quiz.sqlite"	});
+	{dialect: "sqlite", storage: "quiz.sqlite"	});  */
 	
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd, 
